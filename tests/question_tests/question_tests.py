7dd62cbf8919e17594bd8b764b09e2a0cfd6ee43
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-12-14 04:35:47 +0000

    test

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,12 +1,30 @@
-#write function tests here, don't add input('') statements here!
 import unittest
 
-#follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_c.main import get_most_likely_ancestor_consensus
 
-class Test_Config(unittest.TestCase):
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
+class QuestionTests(unittest.TestCase):
 
+    def test_sample_case(self):
+        a, b, c = get_most_likely_ancestor_consensus(
+            "GATATATGCATATACTT",
+            "ATAT"
+        )
+        self.assertEqual(a, 2)
+        self.assertEqual(b, 4)
+        self.assertEqual(c, 10)
+
+    def test_no_matches(self):
+        result = get_most_likely_ancestor_consensus(
+            "AAAAAAAAAAAA",
+            "CGTA"
+        )
+        self.assertEqual(result, ())
+
+    def test_overlapping_matches(self):
+        result = get_most_likely_ancestor_consensus(
+            "AAAAAA",
+            "AAAA"
+        )
+        self.assertEqual(result, (1, 2, 3))
 
